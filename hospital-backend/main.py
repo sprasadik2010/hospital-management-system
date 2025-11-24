da93939b55872097bcee28f6867519cc7a867ddf
SIVA PRASAD I K <sprasadik@gmail.com>
2025-11-24 19:58:02 +0530

    new update

--- hospital-backend/main.py
+++ hospital-backend/main.py
@@ -15,7 +15,7 @@ app = FastAPI(
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://hospital-management-system-frontend-zf6y.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
